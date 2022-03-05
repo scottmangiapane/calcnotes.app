3bb70011205152066b1843b4168520ea09758c7a
Scott Mangiapane <scottmangiapane@gmail.com>
2022-03-05 15:38:52 -0700

    Simplified editor structure and CSS

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,11 @@
-import { createRef, useContext, useEffect } from 'react';
 import Prism from 'prismjs';
+import { createRef, useContext, useEffect } from 'react';
 
 import { AppContext } from './App';
 
 import './Editor.css';
 
 function Editor() {
-  const growWrapRef = createRef();
   const prismRef = createRef();
   const { state, dispatch } = useContext(AppContext);
 
@@ -18,24 +17,21 @@ function Editor() {
 
   function onInput(event) {
     dispatch({ type: 'UPDATE_INPUT', data: event.target.value });
-    growWrapRef.current.dataset.replicatedValue = event.target.value;
   }
 
   return (
     <div id='editor-wrapper'>
-      <div ref={ growWrapRef } className='grow-wrap'>
-        <textarea
-          id='editor-input'
-          autoFocus
-          onInput={ onInput }
-          spellCheck='false'
-        />
-      </div>
       <pre id='editor-highlighting'>
         <code ref={ prismRef } className='language-javascript'>
           { state.text }
         </code>
       </pre>
+      <textarea
+        id='editor-typing'
+        autoFocus
+        onInput={ onInput }
+        spellCheck='false'
+      />
     </div>
   );
 }
