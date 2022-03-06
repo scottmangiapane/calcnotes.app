381c02a24a14c944bfc23321cf4ead8b91d48566
Scott Mangiapane <scottmangiapane@gmail.com>
2022-03-06 16:23:57 -0700

    Updated app name and added favicons

--- src/Titlebar.jsx
+++ src/Titlebar.jsx
@@ -3,7 +3,7 @@ import './Titlebar.css';
 function Titlebar() {
   return (
     <div className='bar center-content titlebar'>
-      <p className='ellipsis'>Notepad Calculator</p>
+      <p className='ellipsis'>CalcNotes</p>
     </div>
   )
 }
