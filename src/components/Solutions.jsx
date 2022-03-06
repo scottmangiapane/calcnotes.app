303fa24c9555cd71f27a92bc950d0488254dbd77
Scott Mangiapane <scottmangiapane@gmail.com>
2022-03-05 18:53:33 -0700

    Removed output color coding; added clipboard functionality

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -6,47 +6,32 @@ import { AppContext } from './App';
 
 import './Solutions.css';
 
-function copyEvent(event) {
-  console.log(event.target.innerText);
-  // const text = event.target.innerText;
-  // clipboard.writeText(text);
-}
-
 function Solutions() {
   const { state } = useContext(AppContext);
   const scope = {};
   const solutions = [];
   const lines = state.text.split('\n');
 
+  function copyEvent(event) {
+    const text = event.target.innerText;
+    navigator.clipboard.writeText(text);
+  }
+
   for (const [index, line] of lines.entries()) {
     let output;
     try {
-        output = evaluate(line, scope);
-    } catch (e) { }
-    let solution;
-    switch (typeof output) {
-      case 'boolean':
-        solution = (
-          <p className='solution solution-boolean' onClick={ copyEvent }>
-            { JSON.stringify(output) }
-          </p>
-        );
-        break;
-      case 'number':
-        const rounded = Math.round(output * 100) / 100;
-        solution = (
-          <p className='solution solution-number' onClick={ copyEvent }>
-            { rounded }
-          </p>
-        );
-        break;
-      default:
-        solution = (
-          <p className='solution solution-string' onClick={ copyEvent }>
-            { JSON.stringify(output) }
-          </p>
-        );
-    }
+      output = evaluate(line, scope);
+    } catch (e) { /* ignored */ }
+    const formatted = (typeof output === 'number')
+      ? Math.round(output * 100) / 100
+      : JSON.stringify(output);
+    const solution = (formatted === undefined)
+    ? <br />
+    : (
+      <p className='solution' onClick={ copyEvent }>
+        { formatted }
+      </p>
+    );
     solutions.push(
       <div key={ index } className='ellipsis solution-wrapper'>
         { solution }
