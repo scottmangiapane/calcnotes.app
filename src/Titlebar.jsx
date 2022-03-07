a04b24cd813607a010d61cdf6115fa7257958815
Scott Mangiapane <scottmangiapane@gmail.com>
2022-03-06 20:25:56 -0700

    Added link to source code

--- src/Titlebar.jsx
+++ src/Titlebar.jsx
@@ -1,10 +1,18 @@
 import './Titlebar.css';
 
 function Titlebar() {
+  const sourceUrl = 'https://github.com/scottmangiapane/calcnotes';
+
   return (
-    <div className='bar center-content titlebar'>
-      <p className='ellipsis'>CalcNotes</p>
-    </div>
+    <p className='titlebar'>
+      <span className='titlebar-item'></span>
+      <span className='titlebar-item'>
+        <span>CalcNotes</span>
+      </span>
+      <span className='titlebar-item titlebar-item-right'>
+        <a href={ sourceUrl } target='_blank'>&lt;/&gt;</a>
+      </span>
+    </p>
   )
 }
 
